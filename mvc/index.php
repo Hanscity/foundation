db93793a943c371b6055cd0ea583442a5c9ddc36
mi <hanscity1111@gmail.com>
2019-02-28 11:10:38 +0800

    start to learn route

---
+++ mvc/index.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @desc 简单实现MVC路由功能
+ * 原始版 copy自：https://blog.csdn.net/Zhihua_W/article/details/53167562
+ * 
+ */
+
+//定义application路径
+define('APPPATH', trim(__DIR__ . '/'));
+
+//获得请求地址
+$root = $_SERVER['SCRIPT_NAME'];
+$request = $_SERVER['REQUEST_URI'];
+$URI = array();
+//获得index.php 后面的地址
+$url = trim(str_replace($root.'?', '', $request), '/');
+
+//如果为空，则是访问根地址
+if (empty($url)) {
+    //默认控制器和默认方法
+    $class = 'index';
+    $func = 'welcome';
+} else {
+    $URI = explode('/', $url);
+
+    //如果function为空 则默认访问index
+    if (count($URI) < 2) {
+        $class = $URI[0];
+        $func = 'index';
+    } else {
+        $class = $URI[0];
+        $func = $URI[1];
+    }
+}
+
+//把class加载进来
+include(APPPATH . 'application/controllers/' . $class . '.php');
+echo APPPATH . 'application/controllers/' . $class . '.php';
+//实例化->将控制器首字母大写
+$obj = ucfirst($class);
+
+call_user_func_array(
+//调用内部function
+    array($obj, $func),
+    //传递参数
+    array_slice($URI, 2)
+);
