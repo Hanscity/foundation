6a2fb18c552d12defbc967a29f28dc5a9c4bdeda
danche <danche@mi.com>
2021-04-11 10:02:31 +0800

    feat: 封装 move 函数

---
+++ front/js/lilichao/tools.js
@@ -0,0 +1,33 @@
+function getStyle(obj, name) {
+    // 这里一定要加上 window, 因为对象的属性没有会报错为 undefined, 而变量没有会直接报错~
+    if (window.getComputedStyle) {
+        return getComputedStyle(obj)[name];
+    } else {
+        // IE 8 的用法
+        return obj.currentStyle[name];
+    }
+}
+
+
+function move(obj, attr, speed, target, callback) {
+
+    clearInterval(obj.timer);
+    
+    if (parseInt(getStyle(obj, attr)) > target) {
+        speed = -speed;
+    }
+
+    obj.timer = setInterval(function () {
+
+        var attrValue = parseInt(getStyle(obj, attr));
+        obj.style[attr] = attrValue + speed + "px";
+
+        if ((speed < 0 && parseInt(attrValue) <= target) || 
+            (speed >= 0 && parseInt(attrValue) >= target) ) {
+
+            obj.style[attr] = target + "px";
+            clearInterval(obj.timer);
+            callback && callback(); 
+        }
+    }, 30);
+}
