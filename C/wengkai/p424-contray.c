7f2d53346b0db34f21b452dc0607c0fdbcb0e9e1
danche <danche@mi.com>
2021-06-09 11:10:14 +0800

    feat: 整数的倒序

--- C/wengkai/p424-contray.c
+++ C/wengkai/p424-contray.c
@@ -2,7 +2,18 @@
 
 int main()
 {
-    int a = 12345;
+    int a = 10000;
 
+    int ret = 0;
+    int res;
+
+    while (a > 0) {
+        res = a % 10;
+        a /= 10;
+        ret = ret * 10 + res;
+    };
+
+    printf("%d", ret);
+    return 0;
     
 }
